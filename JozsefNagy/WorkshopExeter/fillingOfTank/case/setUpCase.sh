302edd7a3419e26c55801e7777f7d18e605d21c2
jnagy <jnagy@24424PC.(none)>
2017-06-01 06:59:07 +0200

    added workshop case and presentation

---
+++ JozsefNagy/WorkshopExeter/fillingOfTank/case/setUpCase.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+gedit 0/alpha.water 0/p_rgh 0/U constant/g constant/dynamicMeshDict constant/turbulenceProperties constant/transportProperties system/controlDict system/fvS*  system/setFieldsDict system/decomposeParDict  foam.foam 
---
+++ JozsefNagy/WorkshopExeter/fillingOfTank/mesh/setUpMesh.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+gedit system/blockMeshDict system/surfaceFeatureExtractDict system/snappyHexMeshDict system/meshQualityDict  foam.foam 
